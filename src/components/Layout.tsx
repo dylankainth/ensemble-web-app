aa9dd9de39adff881ec6c4e690033f7bac6ec8e4
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-09-04 23:55:02 +0000

    feat: added appbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import AppBar from '@/components/AppBar';
 
 const Layout: React.FC = () => {
     return (
         <div>
             <Outlet />
+            <AppBar />
         </div>
     );
 };
