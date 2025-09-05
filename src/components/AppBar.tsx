1c4edaf953388d4a067ef3905e7ef36db607fdbe
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-09-05 00:05:21 +0000

    feat: sorted colours on appbar route dependent

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,29 +1,58 @@
 import { Map, FileClock } from "lucide-react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+
+
 export default function AppBar() {
+    const { pathname } = useLocation()
+
     return (
-
-
         <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
             <div className="grid h-full max-w-lg grid-cols-2 mx-auto font-medium">
+            <Link
+                to="/"
+                className="inline-flex flex-col items-center justify-center px-5 group"
+            >
+                <FileClock
+                className={`w-5 h-5 mb-2 ${
+                    pathname === "/"
+                    ? "text-blue-400"
+                    : "text-gray-500 dark:text-gray-400"
+                } group-hover:text-blue-600 dark:group-hover:text-blue-500`}
+                />
+                <span
+                className={`text-sm ${
+                    pathname === "/"
+                    ? "text-blue-400 font-bold"
+                    : "text-gray-500 dark:text-gray-400"
+                } group-hover:text-blue-600 dark:group-hover:text-blue-500`}
+                >
+                Schedule
+                </span>
+            </Link>
 
-                <Link to="/" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-
-
-                        <FileClock className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-
-                        <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Schedule</span>
-            
-                </Link>
-                <Link to="/map" type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-
-                        <Map className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
-
-                        <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Map</span>
-                 
-                </Link>
+            <Link
+                to="/map"
+                className="inline-flex flex-col items-center justify-center px-5 group"
+            >
+                <Map
+                className={`w-5 h-5 mb-2 ${
+                    pathname === "/map"
+                    ? "text-blue-400"
+                    : "text-gray-500 dark:text-gray-400"
+                } group-hover:text-blue-600 dark:group-hover:text-blue-500`}
+                />
+                <span
+                className={`text-sm ${
+                    pathname === "/map"
+                    ? "text-blue-400 font-bold"
+                    : "text-gray-500 dark:text-gray-400"
+                } group-hover:text-blue-600 dark:group-hover:text-blue-500`}
+                >
+                Map
+                </span>
+            </Link>
             </div>
         </div>
-
     )
 }
+
